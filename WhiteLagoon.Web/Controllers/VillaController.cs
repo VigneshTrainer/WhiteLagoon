c9e74636123414ee3556ebaf2a0a65ba6a76f58f
Vignesh Duraisamy <vignesh.du@Orioninc.com>
2025-05-01 21:43:22 +0530

    Updated

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -14,6 +15,39 @@ namespace WhiteLagoon.Web.Controllers
         {
             var villas =_db.Villas.ToList();
             return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Update(int VillaId)
+        {
+            Villa obj = _db.Villas.FirstOrDefault(u => u.Id == VillaId);
+
+            //var VillaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 0);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
     }
 }
